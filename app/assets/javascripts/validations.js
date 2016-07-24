2dbfb2f3e5ad104f6773f72878f2244f8eaea0d3
Chris Hintz <chrishintz42@gmail.com>
2016-07-23 22:28:10 -0700

    Add in validations

---
+++ app/assets/javascripts/validations.js
@@ -0,0 +1,12 @@
+function validate() {
+  if( document.eulerForm.prime_number.value <= "0") {
+    alert ("Whoops! Please enter a number in the 'Prime Number' field greater than 0");
+    return false;
+  }
+
+  if( document.eulerForm.digits.value <= "0") {
+    alert ("Whoops! Please enter a number in the 'Number of Digits' field greater than 0");
+    return false;
+  }
+  return(true);
+}
